8f64b7ec28955fa1333ddfa9436c36f57d5f0cd8
Lorenzo <lorenzo.coeugniet@hotmail.com>
2020-10-05 21:57:14 +0200

    Identification bug sur le dash stationnaire

--- Pinatatane Prototype/Assets/URP/Scripts/Dash.cs
+++ Pinatatane Prototype/Assets/URP/Scripts/Dash.cs
@@ -23,6 +23,7 @@ namespace Gameplay {
             }
         }
 
+        /* Probleme : la coroutine ne se fait pas dans un fixedUpdate du coup la modif de physique est molle sur le dash stationaire */
         IEnumerator StartDash() {
             speed.value *= 10;
             if (rigidBody.velocity == Vector3.zero) {
--- Pinatatane Prototype/Assets/URP/Scripts/InputManager.cs
+++ Pinatatane Prototype/Assets/URP/Scripts/InputManager.cs
@@ -6,20 +6,6 @@ namespace Gameplay
 {
     public class InputManager : MonoBehaviour
     {
-        /*public static float GetXAxisLeftJoystick() {
-            return Input.GetAxisRaw("Horizontal");
-        }
 
-        public static float GetYAxisLeftJoystick() {
-            return Input.GetAxisRaw("Vertical");
-        }
-
-        public static float GetXAxisRightJoystick() {
-            return Input.GetAxisRaw("RotationX");
-        }
-
-        public static float GetYAxisRightJoystick() {
-            return Input.GetAxisRaw("RotationY");
-        }*/
     }
 }
