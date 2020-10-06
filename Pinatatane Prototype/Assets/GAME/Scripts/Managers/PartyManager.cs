c4f8df8f7f356c71e1fb5af65f7bbfdeadd5cd3b
QuentinROUSSEL <quent.roussel98@gmail.com>
2020-10-06 15:05:40 +0200

    [Quentin] Quelques modifs

---
+++ Pinatatane Prototype/Assets/GAME/Scripts/Managers/PartyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pinatatane
+{
+    public class PartyManager : MonoBehaviour
+    {
+        public static PartyManager Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
---
+++ Pinatatane Prototype/Assets/GAME/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.Events;
+using UnityEngine;
+
+namespace Pinatatane
+{
+    [System.Serializable]
+    public class StateMachine
+    {
+        public List<State> states = new List<State>();
+
+        public int currentStateIndex;
+
+        public bool loop;
+
+        public StateMachine(bool _loop = false)
+        {
+            loop = _loop;
+        }
+
+        public void StartStateMachine()
+        {
+            currentStateIndex = 0;
+            states[currentStateIndex].onEnter?.Invoke();
+        }
+
+        public void NextState()
+        {
+            states[currentStateIndex].onExit?.Invoke();
+            currentStateIndex++;
+
+            if (loop && currentStateIndex == states.Count)
+                currentStateIndex = 0;
+
+            states[currentStateIndex].onEnter?.Invoke();
+        }
+    }
+
+    [System.Serializable]
+    public class State
+    {
+        public UnityEvent
+            onEnter,
+            onExit;
+    }
+}
