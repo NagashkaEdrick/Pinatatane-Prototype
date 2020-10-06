0bb24463a6ad6b3e6ba105b99185d86f23dbf83a
QuentinROUSSEL <quent.roussel98@gmail.com>
2020-10-06 11:37:22 +0200

    [Quentin] Room List

---
+++ Pinatatane Prototype/Assets/GAME/Scripts/Core/TestPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            GetComponent<Rigidbody>().AddForce(new Vector3(10f, 0, 0));
+    }
+}
