a0b35b14301a458a38183a8af054431382325fa5
Lorenzo <lorenzo.coeugniet@hotmail.com>
2020-10-07 12:43:06 +0200

    [Lorenzo] Add SpawnerManager Class

---
+++ Pinatatane Prototype/Assets/GAME/Scripts/Managers/SpawnerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Gere la gestion des spawner et la gestion des demandes de spawn
+ */
+public class SpawnerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
