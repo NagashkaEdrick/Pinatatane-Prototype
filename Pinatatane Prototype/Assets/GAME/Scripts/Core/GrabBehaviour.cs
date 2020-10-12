4fd1c5c3e8887c29395752e2c1ff9b8a62f86f42
Lorenzo <lorenzo.coeugniet@hotmail.com>
2020-10-12 20:57:29 +0200

    [Lorenzo] Grab (ça avance)

--- Pinatatane Prototype/Assets/GAME/Scripts/Core/GrabBehaviour.cs
+++ Pinatatane Prototype/Assets/GAME/Scripts/Core/GrabBehaviour.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Pinatatane
 {
@@ -11,39 +13,25 @@ namespace Pinatatane
         public float duration = 1f;
         public float speed = 10f;
         public Camera _camera;
-        public float aim = 0.5f;
-        public Transform hand;
 
-        public float test = 0.0f;
+        public Image crossHair;
+        [Range(0f, 1f)]
+        public float crossHairPosition = 0.5f;
+        public float length = 15f;
 
-        Vector3 intersection = new Vector3(0, 0, 0);
-        bool grab = false;
+        public int numberOfSegment = 10;
+        private Stack<Vector3> segments = new Stack<Vector3>();
+        private Stack<GameObject> maillons = new Stack<GameObject>();
+
+        /*public GameObject maillon;*/
+
+        int cpt; //nbre de segments deja cree
 
         CharacterMovementBehaviour cc => PlayerManager.Instance.LocalPlayer.characterMovementBehaviour;
         AnimatorBehaviour animator => PlayerManager.Instance.LocalPlayer.animatorBehaviour;
 
         private void Update() {
-            Debug.DrawRay(_camera.transform.position + new Vector3(0, aim, 0), _camera.transform.forward, Color.red);
-            Debug.DrawRay(cc.transform.position, cc.transform.forward, Color.yellow);
-            bool b = LinesIntersection(out intersection,
-                              _camera.transform.position + new Vector3(0, aim, 0), _camera.transform.forward,
-                              cc.transform.position, cc.transform.forward);
-            if (b) Debug.DrawLine(intersection, intersection + new Vector3(0, 1f, 0), Color.red);
-            else Debug.Log("no intersection");
-
-            if (grab && Vector3.Distance(intersection, transform.position) > test) {
-                Debug.Log("go");
-                //transform.position = Vector3.Lerp(transform.position, intersection, speed * Time.deltaTime);
-                transform.forward = intersection - transform.position;
-                transform.Translate(transform.forward * Time.deltaTime * speed);
-            } else if (Vector3.Distance(intersection, transform.position) <= test) {
-                Debug.Log("return");
-                transform.forward = hand.position - transform.position;
-                transform.Translate(transform.forward * Time.deltaTime * speed);
-                //transform.position = Vector3.Lerp(transform.position, hand.position, speed * Time.deltaTime);
-            } else {
-                transform.position = hand.position;
-            }
+            crossHair.transform.position = new Vector3(crossHair.transform.position.x, crossHairPosition * Screen.height, 0);
         }
 
         public void GrabAction()
@@ -61,41 +49,46 @@ namespace Pinatatane
             yield return new WaitForSeconds(animator.animator.GetCurrentAnimatorStateInfo(0).length);
 
             // grab
-            grab = true;
+            segments.Clear();
+            maillons.Clear();
+            segments.Push(transform.position);
+            maillons.Push(transform.GetChild(0).gameObject);
+            cpt = 0;
 
-            yield return new WaitForSeconds(duration);
-            grab = false;
+            Vector3 crossHairPos = new Vector3(crossHair.transform.position.x, crossHair.transform.position.y, 0);
+            Ray ray = _camera.ScreenPointToRay(crossHairPos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, length))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * length, Color.red);
+                if (Input.GetKeyDown(KeyCode.Keypad0)) Debug.Log(hit.collider.name);
+            }
+            else
+            {
+                Vector3 dest = ray.origin + ray.direction.normalized * length;
+                float distance = Vector3.Distance(segments.Peek(), dest); //distance courante entre le grab et la destination
+                float lenghtSegment = distance / numberOfSegment;
+                while (cpt < numberOfSegment)
+                {
+                    yield return new WaitForSeconds(duration / numberOfSegment);
+                    GameObject maillon = maillons.Peek();
+                    GameObject o = Instantiate(maillon, maillon.transform, true);
+                    o.transform.Translate((dest - o.transform.position).normalized * lenghtSegment, Space.World);
+                    maillons.Push(o);
+                    cpt++;
+                }
+                while (cpt > 0)
+                {
+                    yield return new WaitForSeconds(duration / numberOfSegment);
+                    Destroy(maillons.Pop());
+                    cpt--;
+                }
+            }
+
             grabCor = null;
             animator.SetBool("grab", false);
             cc.setMovementActive(true);
             yield break;
-        }
-
-        public static bool LinesIntersection(out Vector3 intersection, Vector3 linePoint1, Vector3 lineVector1, Vector3 linePoint2, Vector3 lineVector2) 
-        {
-
-            Vector3 lineVec3 = linePoint2 - linePoint1;
-            Vector3 crossVec1and2 = Vector3.Cross(lineVector1, lineVector2);
-            Vector3 crossVec3and2 = Vector3.Cross(lineVec3, lineVector2);
-
-            float planarFactor = Vector3.Dot(lineVec3, crossVec1and2);
-
-            //est coplanaire et pas parallele
-            //Debug.Log(planarFactor + "  " + crossVec1and2.sqrMagnitude + "   " + Mathf.Approximately(planarFactor, 0f) + "   " + Mathf.Approximately(crossVec1and2.sqrMagnitude, 0f));
-            /* Probleme: Mathf.Approximately(planarFactor, 0f) retourne false car il y a un minuscule decalage de la camera par rapport au joueur et les 2 vecteur ne sont plus coplanaire
-             *           le decalage est si minime qu'on peux se passer du test, seulement si on utilise pas le slerp, le slerp fais que la camera prend du retard sur la rotation du joueur
-             *           et donc les deux vecteur (forward camera, forward joueur) ne sont plus du tout coplanaire ce qui rapporhce enormement le point d'intersection du joueur
-             *           ce qui provoquerai une distance de grab bien moins grande */
-            if (/*Mathf.Approximately(planarFactor, 0f) &&*/ !Mathf.Approximately(crossVec1and2.sqrMagnitude, 0f))
-            {
-                float s = Vector3.Dot(crossVec3and2, crossVec1and2) / crossVec1and2.sqrMagnitude;
-                intersection = linePoint1 + (lineVector1 * s);
-                return true;
-            } else 
-            {
-                intersection = Vector3.zero;
-                return false;
-            }
         }
     }
 }
