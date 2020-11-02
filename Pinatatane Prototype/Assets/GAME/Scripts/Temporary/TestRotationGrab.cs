ed5cc909121b5fca227314a539760dcd8195e11d
Coeugniet <lorenzo.coeugniet@hotmail.com>
2020-11-02 11:03:34 +0100

    Revert "Grab sur les cotes fonctionnel"
    This reverts commit 525ee2a207f8e2abf1a8989ab202b570a89d9626.

--- Pinatatane Prototype/Assets/GAME/Scripts/Temporary/TestRotationGrab.cs
+++ Pinatatane Prototype/Assets/GAME/Scripts/Temporary/TestRotationGrab.cs
@@ -6,58 +6,34 @@ using UnityEngine;
 public class TestRotationGrab : MonoBehaviour
 {
 
-    public Transform attacker;
-    public SimplePhysic body;
-    public float rotationForce;
-
-    float startAngle;
-    float newAngle;
-    Vector3 attackerTarget;
-    Vector3 attackerForward;
-    Vector3 attackerTarget2;
-    bool link = false;
-    bool locked = false;
-
-    private void Start()
-    {
-        // Calcul de l'angle initial (on ne commence pas forcement la rotation avec un angle de 0 entre l'attaquant et la cible)
-        attackerTarget = transform.position - attacker.position;
-        attackerForward = attacker.forward;
-        attackerTarget.y = 0;
-        attackerForward.y = 0;
-        startAngle = Vector3.SignedAngle(attackerTarget, attackerForward, Vector3.up);
-    }
+    public CharacterMovementBehaviour c;
+    public Rigidbody r;
+    float lastRotation = 0f;
+    float ro;
+    float tan;
+    Coroutine cor = null;
 
     private void Update()
     {
-        if (InputManagerQ.Instance.GetAxis("RotationX") != 0)
-        {
-            link = true;
-            attackerTarget = transform.position - attacker.position;
-            attackerForward = attacker.forward;
-            attackerTarget.y = 0;
-            attackerForward.y = 0;
-            Debug.DrawRay(transform.position, attackerTarget, Color.red);
-            Debug.DrawRay(attacker.position, attackerForward, Color.blue);
-            newAngle = Vector3.SignedAngle(attackerTarget, attackerForward, Vector3.up) - startAngle;
-            attackerTarget2 = Quaternion.Euler(0, newAngle, 0) * attackerTarget;
-            transform.forward = -attacker.forward;
+        if (cor == null && InputManagerQ.Instance.GetAxis("RotationX") != 0) cor = StartCoroutine(rotateCor());
 
-            /* Ce sont des forces donc + la force est faible + la target est avant le point ou elle devait etre sur le cercle autour du joueur
-             * et donc + la prochaine direction tirera vers le joueur et donc + la target se rapprochera vite du joueur
-             * A l'inverse plus la force est forte, plus il s'ecarte du joueur 
-             * (des forces trop fortes cree des bugs car la target s'eloigne trop du cercle et sa fausse les calculs d'angle) 
-             * faire sur le transform de la target transform.Translate(attackerTarget2 - attackerTarget)
-             * place la target parfaitement sur le cercle autour du joueur */
-            Debug.Log("Angle : " + newAngle + "    force added : " + (attackerTarget2 - attackerTarget) * rotationForce);
-            body.AddDirectForce((attackerTarget2 - attackerTarget) * rotationForce);
-        }
-        
-        if (link && !locked && InputManagerQ.Instance.GetAxis("RotationX") == 0)
+    }
+
+    IEnumerator rotateCor()
+    {
+        Debug.Log("Start");
+        while (InputManagerQ.Instance.GetAxis("RotationX") != 0)
         {
-            locked = true;
-            if (newAngle >= 0) body.AddForce(attacker.right * rotationForce);
-            else body.AddForce(-attacker.right * rotationForce);
+            yield return new WaitForSeconds(0.02f);
+            lastRotation %= 360;
+            ro = c.getRotationAngle();
+            transform.RotateAround(c.transform.position, Vector3.up, ro - lastRotation);
+            float tmp = Mathf.Tan((ro - lastRotation) * Mathf.Deg2Rad);
+            if (tmp != 0) tan = tmp;
+            lastRotation = ro;
         }
+        cor = null;
+        Debug.Log("Stop " + tan);
+        r.AddForce(tan * (c.transform.right * 10000 * c.data.rotationAcceleration));
     }
 }
