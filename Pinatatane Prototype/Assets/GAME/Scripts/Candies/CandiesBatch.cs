75297e629847b47c9c7dda36c5f95e1532da0506
QuentinROUSSEL <quent.roussel98@gmail.com>
2020-11-04 17:03:20 +0100

    [Quentin] Reseau clean

--- Pinatatane Prototype/Assets/GAME/Scripts/Candies/CandiesBatch.cs
+++ Pinatatane Prototype/Assets/GAME/Scripts/Candies/CandiesBatch.cs
@@ -29,83 +29,38 @@ namespace Pinatatane
 
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.V))
+                photonView.RPC("InstantiateCandies", RpcTarget.All, 5);
+
+
             if (Input.GetKeyDown(KeyCode.C))
                 Pool(10, Vector3.zero);
         }
 
         public void Pool(int _qte, Vector3 _pos)
         {
-            for (int i = 0; i < _qte; i++)
-            {
-                GameObject go = PhotonNetwork.Instantiate("Candy", _pos, Quaternion.identity);
-                Candy c = go.GetComponent<Candy>();
-                c.Pool(_pos);
-
-                //if (!candies[i].IsPool)
-                //{
-                //    candies[i].IsPool = true;
-                //    candies[i].transform.parent = inGame;
-                //    candies[i].Pool(_pos);
-                //    photonView.RPC("PoolRPC", RpcTarget.All, i, _pos);
-                //}
-            }
-        }
-
-        public void Push(int _ID)
-        {
-            candies[_ID].Push();
-            candies[_ID].transform.parent = pool;
-        }
-
-        public void MoveCandie(int _ID, Vector3 _pos)
-        {
-            photonView.RPC("MoveCandieRPC", RpcTarget.All, _ID, _pos);
-        }
-
-        #region RPC Functions
-        [PunRPC]
-        public void PoolRPC(int _ID, Vector3 _pos)
-        {
-            candies[_ID].IsPool = true;
-            candies[_ID].transform.parent = inGame;
-            candies[_ID].Pool(_pos);
+            //for (int i = 0; i < _qte; i++)
+            //{
+            //    GameObject go = PhotonNetwork.Instantiate("Candy", _pos, Quaternion.identity);
+            //    Candy c = go.GetComponent<Candy>();
+            //    c.name = "RPC Candy";
+            //}
         }
 
         [PunRPC]
-        public void MoveCandieRPC(int _ID, Vector3 _pos)
+        public void InstantiateCandies(int _qte)
         {
-            candies[_ID].transform.position = _pos;
-        }
-        #endregion
-
-#if UNITY_EDITOR
-        [Button]
-        void InstantiateCandies(int quantite)
-        {
-            if (quantite == 0)
+            if (PlayerManager.Instance.IsHosting())
             {
-                for (int i = 0; i < candies.Count; i++)
+                for (int i = 0; i < _qte; i++)
                 {
-                    GameObject c = candies[i].gameObject;
-                    c.SetActive(true);
-                    DestroyImmediate(c);
+                    GameObject go = PhotonNetwork.Instantiate("Candy", new Vector3(500, 500, 500), Quaternion.identity);
+                    Candy c = go.GetComponent<Candy>();
+                    candies.Add(c);
+                    c.name = "RPC Candy";
+                    c.gameObject.SetActive(false);
                 }
-                candies.Clear();
             }
-
-            for (int i = 0; i < quantite; i++)
-            {
-                Candy c = PrefabUtility.InstantiatePrefab(candyPrefab) as Candy;
-                c.transform.position = new Vector3(500, 0, 500);
-                c.transform.parent = pool;
-                candies.Add(c);
-                candies[i].ID = i;
-
-                c.IsPool = false;
-            }
-
-            EditorUtility.SetDirty(this);
         }
-#endif
     }
 }
