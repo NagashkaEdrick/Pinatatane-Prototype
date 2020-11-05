3aa72db7675b0638dcfa5ce263000bb54c60b851
QuentinROUSSEL <quent.roussel98@gmail.com>
2020-11-05 11:19:51 +0100

    [Quentin] Pinata Override Control

---
+++ Pinatatane Prototype/Assets/GAME/Scripts/Core/Player/PinataOverrideControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using Sirenix.OdinInspector;
+using UnityEngine.Events;
+using QRTools.Inputs;
+
+namespace Pinatatane
+{
+    public class PinataOverrideControl : MonoBehaviour
+    {
+        [SerializeField] Pinata myPinata;
+        public PhotonView photonView;
+        [SerializeField] QInputs[] controlsInputs = default;
+
+        public bool isOverrided = false;
+
+        [PunRPC]
+        public void RPCWinControl(int _ID)
+        {
+            if (photonView.ViewID == _ID)
+            {
+                isOverrided = false;
+            }
+        }
+
+        [PunRPC]
+        public void RPCLoseControl(int _ID)
+        {
+            if (photonView.ViewID == _ID)
+            {
+                isOverrided = true;
+            }
+        }
+    }
+}
